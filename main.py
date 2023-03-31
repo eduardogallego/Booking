d5094224e95b8b4cc978b2654637b5fd42390db3
Eduardo Gallego <eduardo.gallego@bluu.es>
2023-03-31 03:30:39 +0200

    Show court status

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from security import Security
 
 config = Config()
 
-force_update = True
+# security = Security(config)
+# security.update_credentials()
+
+force_update = False
 if not force_update and os.path.isfile(config.get('credentials_file')):
     update_credentials = False
     with open(config.get('credentials_file')) as input_file:
--- server.py
+++ server.py
@@ -4,12 +4,13 @@ import time
 
 from apiclient import ApiClient
 from config import Config
-from datetime import date
-from flask import Flask, render_template, send_from_directory
+from datetime import date, datetime, timedelta
+from flask import Flask, render_template, request, send_from_directory
 from security import Security
 
 app = Flask(__name__)
 config = Config()
+status_cache = {}
 
 if os.path.isfile(config.get('credentials_file')):
     update_credentials = False
@@ -32,7 +33,7 @@ if update_credentials:
 apiclient = ApiClient(config, credentials)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     day = date.today()
     day_str = day.strftime('%A %-d %B')
@@ -40,25 +41,66 @@ def index():
     return render_template("index.html", day=day_str, court_status=court_status)
 
 
-@app.route('/calendar')
+@app.route('/calendar', methods=['GET'])
 def calendar():
     today = date.today()
     today_str = today.strftime('%Y-%m-%d')
     return render_template("calendar.html", today=today_str)
 
 
-@app.route('/book/<date>')
-def book(date):
-    return render_template("book.html", date=date)
+@app.route('/events', methods=['GET'])
+def events():
+    args = request.args
+    start = args['start'].split('T', 1)[0]
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    end = args['end'].split('T', 1)[0]
+    end_date = datetime.strptime(end, '%Y-%m-%d')
+    request_date = start_date
+    result = []
+    now = datetime.now()
+    while request_date < end_date:
+        if request_date.replace(hour=10) > now + timedelta(days=1):
+            break
+        request_date_str = request_date.strftime('%Y-%m-%d')
+        if request_date_str in status_cache:
+            result += status_cache.get(request_date_str)
+            request_date += timedelta(days=1)
+            continue
+        print('Request date: %s' % request_date)
+        booked_events = []
+        court_status = apiclient.check_court_status(request_date)
+        for hour, available in court_status.items():
+            for court in ['court1', 'court2']:
+                if available[court] == '0':
+                    if court == 'court1':
+                        event_start = request_date.replace(hour=int(hour))
+                        event_end = request_date.replace(hour=int(hour), minute=30)
+                    else:   # court2
+                        event_start = request_date.replace(hour=int(hour), minute=30)
+                        event_end = request_date.replace(hour=int(hour) + 1,)
+                    booked_events.append({
+                        "start": event_start.strftime('%Y-%m-%dT%H:%M:%S'),
+                        "end": event_end.strftime('%Y-%m-%dT%H:%M:%S'),
+                        "display": "background", "color": "#ff9f89"})
+        if now > request_date.replace(hour=22):
+            status_cache[request_date_str] = booked_events
+        result += booked_events
+        request_date += timedelta(days=1)
+    return result
 
 
-@app.route('/favicon.ico')
+@app.route('/book/<book_timestamp>', methods=['GET'])
+def book(book_timestamp):
+    return render_template("book.html", date=book_timestamp)
+
+
+@app.route('/favicon.ico', methods=['GET'])
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'images'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-@app.route('/images/<image>')
+@app.route('/images/<image>', methods=['GET'])
 def get_image(image):
     return send_from_directory(os.path.join(app.root_path, 'images'), image)
 
